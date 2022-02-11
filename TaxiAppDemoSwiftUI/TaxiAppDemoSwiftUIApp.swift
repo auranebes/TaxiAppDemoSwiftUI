5b2808de8f76126565dbaa52f2bd3c42c477757e
Arslan Abdullaev <ars352@yahoo.com>
2022-02-11 14:23:48 +0300

    Initial Commit

---
+++ TaxiAppDemoSwiftUI/TaxiAppDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppDemoSwiftUIApp.swift
+//  TaxiAppDemoSwiftUI
+//
+//  Created by Arslan Abdullaev on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
